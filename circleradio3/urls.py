e986c67ba025bb11f7e574f329025d1cc9ad96d7
ekayesorko <ekayesorko@gmail.com>
2021-04-29 00:49:29 +0600

    favicon debug

--- circleradio3/urls.py
+++ circleradio3/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('profiles/', include('profiles.urls')),
     path('radio/', include('radio.urls')),
-    path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('img/favicon.ico'))),
+    path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('favicon.ico'))),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
