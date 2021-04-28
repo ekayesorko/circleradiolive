7fff650c7998aa67bc226940cbb9d36f03dcb2c6
ekayesorko <ekayesorko@gmail.com>
2021-04-29 02:38:20 +0600

    static root bug fix

--- circleradio3/settings.py
+++ circleradio3/settings.py
@@ -129,8 +129,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static_project'),
 ]
 
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn", "static_root")
+STATIC_ROOT = os.path.join(BASE_DIR, "static_cdn", "static_root")
 
-MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn", "media_root")
+MEDIA_ROOT = os.path.join(BASE_DIR, "static_cdn", "media_root")
 
 LOGIN_URL = 'profiles:login_view'
